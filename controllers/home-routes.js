746e57474119335e8469c8dae476542958a8ea4c
Chad Branagan <chad.branagan@gmail.com>
2021-12-15 20:25:49 -0800

    still need put and delete routes as well as user authentication

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
+
 
 router.get('/', (req, res) => {
-    res.json({ 'message': 'hello' });
+    res.json({ message: 'hello' });
 });
 
 module.exports = router;
