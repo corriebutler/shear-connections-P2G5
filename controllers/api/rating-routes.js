e60ea6d7a1827531345968ea9764bd98d22b63ce
Chad Branagan <chad.branagan@gmail.com>
2021-12-28 09:24:49 -0800

    completed routes to return all reviews about a cosmo and all reviews by a user

--- controllers/api/rating-routes.js
+++ controllers/api/rating-routes.js
@@ -23,6 +23,39 @@ router.get('/:id', (req, res) => {
     })
 })
 
+
+// get all reviews by a user
+router.get('/user/:id', (req, res) => {
+    Rating.findAll({
+        where: {
+            user_id: req.params.id
+        }
+    })
+    .then(ratingData => res.json(ratingData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+    
+})
+
+//get all reviews about a comso
+router.get('/cosmo/:id', (req, res) => {
+    Rating.findAll({
+        where: {
+            cosmo_id: req.params.id
+        }
+    })
+    .then(ratingData => res.json(ratingData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+    
+})
+
+
+
 router.post('/', (req, res) => {
     Rating.create({
         value: req.body.value,
