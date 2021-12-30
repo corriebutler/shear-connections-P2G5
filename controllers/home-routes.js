e9950b85b31ae213e7e7531f26ee1229f0564bf0
Chad Branagan <chad.branagan@gmail.com>
2021-12-29 17:44:22 -0800

    complete merge

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,22 @@
-const router = require('express').Router();
-const path = require('path');
-const { Cosmo } = require('../models');
+const router = require("express").Router();
+const path = require("path");
+const { Cosmo } = require("../models");
 
+router.get("/home", (req, res) => {
+  Cosmo.findAll({})
+    .then((cosmoData) => {
+      const cosmos = cosmoData.map((cosmo) => cosmo.get({ plain: true }));
 
-
-router.get('/home', (req, res) => {
-    Cosmo.findAll({
-
-    }).then(cosmoData => {
-        const cosmos = cosmoData.map(cosmo => cosmo.get({ plain: true }));
-        
-        res.render('homepage', { cosmos });
+      res.render("homepage", { cosmos });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-    
-})
-
-router.get('/login', (req, res) => {
-    res.render('login');
-  })
-    
-=======
-router.get('/', (req, res) => {
-    res.json({ message: 'hello' });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-
-router.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/sign-up-modal.html'))
+router.get("/login", (req, res) => {
+  res.render("login");
 });
-
-// router.get('tranquil-sea-30993.herokuapp.com/', (req, res) => {
-//     res.json({ message: 'hello' });
-// });
 
 module.exports = router;
